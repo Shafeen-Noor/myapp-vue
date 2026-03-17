24382741df563547e16ad1a865f9ef75c5fc8b8f
Shafeen Noor <72748751+Shafeen-Noor@users.noreply.github.com>
2026-03-17 21:09:04 +0500

    Counter

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { counter } from './counter'
+const { count, increment } = counter()
+</script>
+
+<template>
+  <h1>Vue+JS Counter</h1>
+  <button @click="increment">Clicked {{ count }} {{ count === 1 ? 'time' : 'times' }}</button>
+</template>
+
+<style scoped></style>
