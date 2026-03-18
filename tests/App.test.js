a4c8f03d82b548132f304102c526a1be44f1ec59
Shafeen Noor <72748751+Shafeen-Noor@users.noreply.github.com>
2026-03-18 21:32:22 +0500

    added tests

---
+++ tests/App.test.js
@@ -0,0 +1,29 @@
+import { describe, test, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import App from '../src/App.vue'
+
+describe('App.vue', () => {
+  test('renders initial count', () => {
+    const wrapper = mount(App)
+    expect(wrapper.text()).toContain('Clicked 0 times')
+  })
+
+  test('increments when button is clicked', async () => {
+    const wrapper = mount(App)
+    const button = wrapper.find('button')
+
+    await button.trigger('click')
+    await button.trigger('click')
+
+    expect(wrapper.text()).toContain('Clicked 2 times')
+  })
+
+  test('shows correct singular/plural text', async () => {
+    const wrapper = mount(App)
+    const button = wrapper.find('button')
+
+    await button.trigger('click')
+
+    expect(wrapper.text()).toContain('Clicked 1 time')
+  })
+})
