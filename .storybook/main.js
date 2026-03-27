26ae80ad0c736ecbdc1d8ede9c2138035ddb8499
Shafeen Noor <72748751+Shafeen-Noor@users.noreply.github.com>
2026-03-28 00:22:35 +0500

    Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [],
+  framework: '@storybook/vue3-vite',
+}
+export default config
